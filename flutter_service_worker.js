2e2c31d7ab343de3bd91cb8baf030005dae1784a
0xmove <0xmovebuilder@gmail.com>
2024-05-09 16:15:21 +0800

    Built web
    Branch: main
    Commit: 59731cd2139eb821388583b1f9c78b237bab50d3
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "519dd582249e4ebd677b9404fa910771",
-"index.html": "d5d6c70e8e634f5d88960b9e85ba4079",
-"/": "d5d6c70e8e634f5d88960b9e85ba4079",
-"main.dart.js": "a5b45954fc59a5cb56cb51e4a2382503",
+"index.html": "0a3c908d4fcca0dfbacbf4e07ea3ad03",
+"/": "0a3c908d4fcca0dfbacbf4e07ea3ad03",
+"main.dart.js": "330c153b97690f5606c6b6402cf4adef",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "web_support.js": "509ae636cfdd93e49b5a6eaf0f06d79f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
